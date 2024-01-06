981b1aa171fb6b0b3ff0a562be446b54a3521db7
rethikaskumar <rethikaskumar@gmail.com>
2024-01-06 10:10:32 +0530

    level0

--- level0/level0.py
+++ level0/level0.py
@@ -1,6 +1,6 @@
 import json
 
-f=open('Input data\\level0.json')
+f=open('C:\\21pt23\\Input data\\level0.json')
 data=json.load(f)
 
 distances={}
@@ -36,18 +36,25 @@ def MST(dist, start):
 
 output={}
 
-for start in distances.keys():
-    output[start]={}
+for vehicle in data['vehicles'].keys():
+    output[vehicle]={}
     visited.clear()
+    rest=data['vehicles'][vehicle]['start_point']
+    rdist={}
+    for i in range(len(data['restaurants'][rest]["neighbourhood_distance"])):
+        next='n'+str(i)
+        rdist[next]=data['restaurants'][rest]["neighbourhood_distance"][i]
+    rdist=dict(sorted(rdist.items(), key=lambda x:x[1]))
+    start=list(rdist.keys())[0]
     path=MST(distances, start)
-    path.insert(0, 'r0')
-    path.append('r0')
-    output[start]['path']=path
+    path.insert(0, rest)
+    path.append(rest)
+    output[vehicle]['path']=path
 
 for(key, value) in output.items():
     print(key, value)
 
-outfile=open('output0.json', "w")
+outfile=open('level0\\output0.json', "w")
 json.dump(output, outfile, indent=5)
 
 
